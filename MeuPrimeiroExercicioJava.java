8743f0b9655aba4e24163e462b0a785956ad66c2
JhoNBaratheoN <jhonatan.oliveira931002@gmail.com>
2022-01-25 21:39:08 -0300

    um commit qualquer

---
+++ MeuPrimeiroExercicioJava.java
@@ -0,0 +1,17 @@
+class MeuPrimeiroExercicioJava
+{
+	public static void main (String entrada[])
+	{
+		int idade = 28;
+		char genero = 'M';
+		double peso = 93.0;
+		String nome = "Jhonatan";
+		String curso = "Analise e Desenvolvimento de Sistemas";
+		
+		System.out.println("Sou o " + nome + " tenho " + idade + " anos e peso " + peso + "kg. Estudo " + curso + " na Faculdade Descomplica.");
+		
+		System.exit(0);
+			
+	}
+	
+}
